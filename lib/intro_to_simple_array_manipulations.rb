af6da3ca362e1dcc60cb0897a40a4916254f10cf
Reilly Hartigan <reilly.hartigan@yale.edu>
2019-04-04 22:37:43 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,27 @@
+def using_push(array, string)
+array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  pop.array
+end
+
+def pop_with_args(array, 2)
+  pop.array(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array, 2)
+  array.shift(2)
+end
+
+def using_concat(array, array2)
+  array.concat(array2)
+end
